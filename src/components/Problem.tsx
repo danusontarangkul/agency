479380c2da66b9efd43df1d4a07d054c3cf90fb0
David  Anusontarangkul <davidanusontarangkul@Davids-MacBook-Pro.local>
2024-07-24 14:49:56 -0700

    update video

--- src/components/Problem.tsx
+++ src/components/Problem.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import { BentoGrid, BentoGridItem } from "./BentoGrid";
 
 const items = [
@@ -50,23 +51,47 @@ const items = [
 ];
 
 export default function Problem() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div className="px-10 md:py-20 py-10 bg-custom-bg ">
       <h2 className="text-gray-50 text-center font-bold text-4xl md:text-5xl lg:text-6xl mb-6 md:mb-20">
         <span className="text-customColor2">Attention</span> Personal Brands!
       </h2>
-      <BentoGrid className="max-w-2xl mx-auto" rowHeight="">
-        {items.map((item, i) => (
-          <BentoGridItem
-            key={i}
-            title={item.title}
-            description={item.description}
-            header={item.header}
-            className={`border border-gray-300 p-4 `}
-            size={item.size}
-          />
-        ))}
-      </BentoGrid>
+      <div className="flex flex-col md:flex-row-reverse justify-evenly">
+        <div className="flex justify-center mb-8 md:mb-0">
+          {isClient ? (
+            <video
+              className=" h-[600px]"
+              controls
+              poster="/videos/intro-thumbnail.webp"
+            >
+              <source src={"/videos/Intro.mp4"} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            <div className="h-[600px] bg-gray-200"></div> // Placeholder to maintain layout
+          )}
+        </div>
+        <div className="w-full md:w-1/2">
+          <BentoGrid className="" rowHeight="">
+            {items.map((item, i) => (
+              <BentoGridItem
+                key={i}
+                title={item.title}
+                description={item.description}
+                header={item.header}
+                className={`border border-gray-300 p-4 `}
+                size={item.size}
+              />
+            ))}
+          </BentoGrid>
+        </div>
+      </div>
     </div>
   );
 }
